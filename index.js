ac1c94dfaa17952f95883bbddff4f8f85d2635a5
Michael <michael.alexander0008@gmail.com>
2023-11-22 16:10:58 +0000

    Complete Exercise 3.3

--- index.js
+++ index.js
@@ -37,6 +37,19 @@ app.get('/info', (request, response) => {
   `)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+    return;
+  } else {
+    response.status(404).end();
+    return;
+  }
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
