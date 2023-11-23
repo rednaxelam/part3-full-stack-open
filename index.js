0ea919a541b3c1977975638df5650910188459e6
Michael <michael.alexander0008@gmail.com>
2023-11-23 01:25:08 +0000

    Complete Exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+morgan.token('json-data', (req, res) => JSON.stringify({'name':req.body.name, 'number':req.body.number}));
+app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :json-data'));
 
 let persons = [
   { 
