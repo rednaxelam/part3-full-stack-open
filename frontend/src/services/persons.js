ca836f2331420ae95cc28173f5930839c6e923c6
Michael <michael.alexander0008@gmail.com>
2023-11-28 00:42:40 +0000

    Complete Exercise 3.10

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:3001/api/persons';
+const baseURL = 'https://fso-part-3-backend.fly.dev/api/persons';
 
 const createContact = newContact => {
   return axios.post(baseURL, newContact).then(response => response.data);
