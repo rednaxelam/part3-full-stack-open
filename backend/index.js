c3fb95e7243df01a119614a8757de6504f18d68f
Michael <michael.alexander0008@gmail.com>
2023-12-15 00:55:28 +0000

    Complete Exercise 3.13

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.json());
 app.use(cors());
@@ -10,35 +12,9 @@ app.use(express.static('dist'));
 morgan.token('json-data', (req, res) => JSON.stringify({'name':req.body.name, 'number':req.body.number}));
 app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :json-data'));
 
-let persons = [
-  { 
-    id: 1,
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: 2,
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: 3,
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: 4,
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
-];
-
-let generateID = () => {
-  return Math.floor(Math.random()*Number.MAX_SAFE_INTEGER);
-}
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => response.json(persons))
 })
 
 app.get('/info', (request, response) => {
@@ -49,16 +25,7 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    response.json(person);
-    return;
-  } else {
-    response.status(404).end();
-    return;
-  }
+  Person.findById(request.params.id).then(person => response.json(person));
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -99,7 +66,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
