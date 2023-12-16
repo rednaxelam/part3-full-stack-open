23148d82b85d612b94fb6089a1693f0bce29346b
Michael <michael.alexander0008@gmail.com>
2023-12-16 20:03:39 +0000

    Complete Exercise 3.15

--- backend/index.js
+++ backend/index.js
@@ -29,11 +29,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-  
-  response.status(204).end();
-  return;
+  Person.findByIdAndDelete(request.params.id)
+    .then(deletedDocument => {
+      response.status(204).end();
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
